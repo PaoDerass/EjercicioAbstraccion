b2f0ee9986444251a7c2b2a74bf8c8bb925c879d
PaoDerass <paola_deras@unitec.edu>
2024-08-02 21:07:38 -0600

    creacion clase matematicas

---
+++ EjercicioAbstraccion/Matematicas.cs
@@ -0,0 +1,21 @@
+﻿
+namespace EjercicioAbstraccion
+{
+    public abstract class Matematicas
+    {
+        public abstract double potencia(double Base, double exponente);
+
+        public abstract double Logaritmo(double Numero);
+
+        public abstract double Raiz(double Numero);
+
+        public abstract double Exponencial(double Numero);
+
+        public void Imprimir(string Operacion, double resultado)
+        {
+        Console.WriteLine($"{Operacion}:{resultado}");
+}
+
+
+    }
+}
